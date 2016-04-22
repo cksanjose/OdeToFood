402cf8765501aa2d9779ce15451328b248589056
cksanjose <cksanjose@gmail.com>
2016-04-22 14:53:52 -0500

    app settings

--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -5,30 +5,46 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OdeToFood.Services;
 
 namespace OdeToFood
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		public Startup()
+		{
+			var builder = new ConfigurationBuilder()
+					.AddJsonFile("appSettings.json");
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseIISPlatformHandler();
+			Configuration = builder.Build();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+		}
 
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
-    }
+		public IConfiguration Configuration { get; set; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddSingleton(provider => Configuration);
+			services.AddSingleton<IGreeter, Greeter>();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IGreeter greeter)
+		{
+			app.UseIISPlatformHandler();
+
+			app.Run(async (context) =>
+			{
+				var greeting = greeter.GetGreeting();
+				await context.Response.WriteAsync(greeting);
+			});
+		}
+
+		// Entry point for the application.
+		public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+	}
 }
