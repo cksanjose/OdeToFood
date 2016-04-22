f0f6575067bf2517f04c4831204db1a9d4d2a4c2
cksanjose <cksanjose@gmail.com>
2016-04-22 15:12:12 -0500

    Diagnostics

--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -32,10 +32,12 @@ namespace OdeToFood
 			services.AddSingleton<IGreeter, Greeter>();
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		// This method gets called by the runtime. 
+		// Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IGreeter greeter)
 		{
 			app.UseIISPlatformHandler();
+			app.UseRuntimeInfoPage();
 
 			app.Run(async (context) =>
 			{
